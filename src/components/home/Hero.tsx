75750bad8a620de960e820b3c05c9b8143b1944e
Priyansh Rai <priyanshrai16@gmail.com>
2023-12-14 19:40:09 +0530

    Modified theme by adjusting css variables

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,3 +1,5 @@
+import { Button } from "../ui/button";
+
 type HeroProps = {};
 
 const Hero = ({}: HeroProps) => {
@@ -6,6 +8,8 @@ const Hero = ({}: HeroProps) => {
       <h1 className="text-5xl w-1/2 leading-normal font-medium">
         Manage your funds easily with{" "}
         <span className="text-foreground">MoneyDex</span>
+        <Button variant={"default"}>Primary</Button>
+        <Button variant={"secondary"}>Secondary</Button>
       </h1>
     </div>
   );
