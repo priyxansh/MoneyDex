175611650d231d7f010493098303eb661fa14dfc
Priyansh Rai <priyanshrai16@gmail.com>
2023-12-21 12:39:02 +0530

    Add account creation when new user is created in next-auth.ts

--- src/lib/next-auth.ts
+++ src/lib/next-auth.ts
@@ -78,6 +78,13 @@ export const authOptions: NextAuthOptions = {
                 },
               ],
             },
+            accounts: {
+              create: [
+                {
+                  name: "Primary",
+                },
+              ],
+            },
           },
         });
 
