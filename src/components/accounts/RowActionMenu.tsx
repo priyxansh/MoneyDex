4c83e9132fef069cf22e4b554047670b35c9db29
Priyansh Rai <priyanshrai16@gmail.com>
2023-12-22 13:12:07 +0530

    Update RowActionMenu component to include a warning about deleting related transactions

--- src/components/accounts/RowActionMenu.tsx
+++ src/components/accounts/RowActionMenu.tsx
@@ -66,7 +66,9 @@ const RowActionMenu = ({ accountId }: RowActionMenuProps) => {
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Are you sure?</DialogTitle>
-          <DialogDescription>This action cannot be undone.</DialogDescription>
+          <DialogDescription>
+            This will also delete all transactions related to this account.
+          </DialogDescription>
           <div className="flex flex-col sm:flex-row gap-4 items-center justify-center sm:justify-end mt-2">
             <DialogClose asChild>
               <Button variant="secondary" className="w-full sm:w-auto">
