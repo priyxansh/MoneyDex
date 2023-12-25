93bc6a5f5fc1abf9e541691ac538f54a13388a30
Priyansh Rai <priyanshrai16@gmail.com>
2023-12-25 23:03:52 +0530

    Separated category type select to a new component

--- src/components/categories/CategoriesFilter.tsx
+++ src/components/categories/CategoriesFilter.tsx
@@ -1,58 +1,13 @@
-"use client";
-
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-
-import { useRouter, useSearchParams } from "next/navigation";
 import CategorySearch from "./CategorySearch";
+import CategoryTypeSelect from "./CategoryTypeSelect";
 
 type CategoriesFilterProps = {};
 
 const CategoriesFilter = ({}: CategoriesFilterProps) => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
-  const searchQuery = searchParams.get("search");
-
   return (
     <div className="flex gap-4 flex-wrap items-center my-2">
       <CategorySearch />
-      <div className="flex gap-2 items-center flex-wrap">
-        <span className="text-sm">Category type:</span>
-        <Select
-          onValueChange={(value) => {
-            if (searchQuery) {
-              router.push(
-                `/categories?search=${searchQuery}${
-                  value === "ALL" ? "" : `&type=${value.toLowerCase()}`
-                }`
-              );
-              return;
-            }
-
-            router.push(
-              `/categories${
-                value === "ALL" ? "" : `?type=${value.toLowerCase()}`
-              }`
-            );
-          }}
-          defaultValue={searchParams.get("type")?.toUpperCase() ?? "ALL"}
-        >
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Type" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="ALL">All</SelectItem>
-            <SelectItem value="INCOME">Income</SelectItem>
-            <SelectItem value="EXPENSE">Expense</SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
+      <CategoryTypeSelect />
     </div>
   );
 };
---
+++ src/components/categories/CategoryTypeSelect.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { useRouter, useSearchParams } from "next/navigation";
+
+type CategoryTypeSelectProps = {};
+
+const CategoryTypeSelect = ({}: CategoryTypeSelectProps) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const searchQuery = searchParams.get("search");
+
+  return (
+    <div className="flex gap-2 items-center flex-wrap">
+      <span className="text-sm">Category type:</span>
+      <Select
+        onValueChange={(value) => {
+          if (searchQuery) {
+            router.push(
+              `/categories?search=${searchQuery}${
+                value === "ALL" ? "" : `&type=${value.toLowerCase()}`
+              }`
+            );
+            return;
+          }
+
+          router.push(
+            `/categories${
+              value === "ALL" ? "" : `?type=${value.toLowerCase()}`
+            }`
+          );
+        }}
+        defaultValue={searchParams.get("type")?.toUpperCase() ?? "ALL"}
+      >
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Type" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="ALL">All</SelectItem>
+          <SelectItem value="INCOME">Income</SelectItem>
+          <SelectItem value="EXPENSE">Expense</SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
+export default CategoryTypeSelect;
