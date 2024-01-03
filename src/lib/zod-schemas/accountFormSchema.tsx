06df1ae1e44abcb6707d288aabc611a90839ff66
Priyansh Rai <priyanshrai16@gmail.com>
2024-01-03 23:16:33 +0530

    Update balance validation in accountFormSchema

--- src/lib/zod-schemas/accountFormSchema.tsx
+++ src/lib/zod-schemas/accountFormSchema.tsx
@@ -4,9 +4,8 @@ export const accountFormSchema = z.object({
   accountName: z.string().min(1).max(50),
   balance: z.coerce
     .number({ invalid_type_error: "Balance must be a number." })
-    .min(-1000000000)
-    .max(1000000000)
-    .transform((value) => {
-      return +value;
+    .min(0, { message: "Balance must be greater than or equal to 0." })
+    .max(1000000000, {
+      message: "Balance must be less than or equal to 1,000,000,000.",
     }),
 });
