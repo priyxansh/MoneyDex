3494899d27ddf5c876c8e0dc1c737a6979c0d64b
Priyansh Rai <priyanshrai16@gmail.com>
2024-01-05 23:02:31 +0530

    Refactor HeroButtons.tsx

--- src/components/home/HeroButtons.tsx
+++ src/components/home/HeroButtons.tsx
@@ -1,26 +1,26 @@
 import { authOptions } from "@/lib/next-auth";
-import { Session, getServerSession } from "next-auth";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { Button } from "../ui/button";
 
 type HeroButtonsProps = {};
 
 const HeroButtons = async ({}: HeroButtonsProps) => {
-  const session = (await getServerSession(authOptions)) as Session;
+  const session = await getServerSession(authOptions);
+
+  if (!session)
+    return (
+      <Button asChild>
+        <Link href={"/auth/signin"}>Get Started</Link>
+      </Button>
+    );
 
   return (
     <>
-      {!session && (
-        <Button asChild>
-          <Link href={"/auth/signin"}>Get Started</Link>
-        </Button>
-      )}
-      {session && <Button variant={"secondary"}>Edit Profile</Button>}
-      {session && (
-        <Button asChild>
-          <Link href={"/dashboard"}>Dashboard</Link>
-        </Button>
-      )}
+      <Button variant={"secondary"}>Edit Profile</Button>
+      <Button asChild>
+        <Link href={"/dashboard"}>Dashboard</Link>
+      </Button>
     </>
   );
 };
