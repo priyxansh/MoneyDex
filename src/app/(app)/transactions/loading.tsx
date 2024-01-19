5e4571ad64312d87c6c1260c3ba7dc7e2ed80849
Priyansh Rai <priyanshrai16@gmail.com>
2024-01-19 22:14:32 +0530

    Add loading.tsx for categories and transactions pages

--- src/app/(app)/transactions/loading.tsx
+++ src/app/(app)/transactions/loading.tsx
@@ -9,13 +9,11 @@ const Loading = ({}: LoadingProps) => {
   return (
     <div className="flex flex-col flex-grow">
       <div className="flex justify-between gap-4 flex-wrap">
-        <h1 className="text-2xl font-semibold">Categories</h1>
-        <Button variant="default" asChild className="w-full sm:w-auto">
-          <Link href={"/categories/new"}>
-            <span>
-              <PlusIcon className="w-5 h-5 mr-2" />
-            </span>
-            <span>Create Category</span>
+        <h1 className="text-2xl font-semibold">Transactions</h1>
+        <Button asChild className="w-full sm:w-auto">
+          <Link href="/transactions/new">
+            <PlusIcon className="w-5 h-5 mr-2" />
+            <span>New Transaction</span>
           </Link>
         </Button>
       </div>
